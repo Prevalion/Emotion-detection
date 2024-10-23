1321b2afb692919d926b69648d07982167e55e1f
Prevalion <medayoubgh0@gmail.com>
2024-10-23 18:39:22 +0100

    remove reportmissingimport warnings

--- src/emotions.py
+++ src/emotions.py
@@ -2,12 +2,12 @@ import numpy as np
 import argparse
 import matplotlib.pyplot as plt
 import cv2
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Dropout, Flatten
-from tensorflow.keras.layers import Conv2D
-from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.layers import MaxPooling2D
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.models import Sequential # type: ignore
+from tensorflow.keras.layers import Dense, Dropout, Flatten # type: ignore
+from tensorflow.keras.layers import Conv2D # type: ignore
+from tensorflow.keras.optimizers import Adam # type: ignore
+from tensorflow.keras.layers import MaxPooling2D # type: ignore
+from tensorflow.keras.preprocessing.image import ImageDataGenerator # type: ignore
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
