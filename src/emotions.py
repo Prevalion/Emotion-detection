41e663d241827353311a22391780c3cd3d9ce5e8
Prevalion <medayoubgh0@gmail.com>
2024-12-10 01:20:00 +0100

    change to 48*48
    resize the dataset's pictures to respect the model's architecture

--- src/emotions.py
+++ src/emotions.py
@@ -85,6 +85,8 @@ model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.25))
 
 model.add(Flatten())
